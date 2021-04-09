3010c938643b7d13d75ef506599883f0e2ebcf02
Alexander Castro <Alexcast129@gmail.com>
2021-04-09 12:59:18 -0400

    Starting point

---
+++ Finanical_Functions.py
@@ -0,0 +1,46 @@
+def HPR(Beginning_Price, End_Value, Dividend):
+#Holding Period Return Variables
+    # Beginning_Price = float(input("What was the purchase price of the security? "))
+    # End_Value = float(input("what was the sale price of the security? "))
+    # Dividend = float(input("How much in dividend payments were recieved? "))
+    B = Beginning_Price
+    if B < 0:
+        B = abs(B)
+    E = End_Value
+    if E < 0:
+        E = abs(E)
+    D = Dividend
+    if D < 0:
+        D = abs(D)
+#Holding Period Return math
+    HPR= ((E-B+D)/B)
+    HPR= HPR*100
+    return print("Holding Period Return is ","%6.2f" % HPR,"%")
+
+##########################################
+def APR(Per_Period_Rate, Number_Periods):
+#APR Formula Variables
+    # Per_Period_Rate = float(input("What is the rate per period? "))
+    # Number_Periods= float(input("How many periods are there? "))
+    P1 = Per_Period_Rate
+    N1 = Number_Periods
+#APR Math
+    APR = (P1*N1)
+    return print("The APR is ","%6.2f" % APR,"%")
+
+##########################################
+
+def EAR(Per_Period_Rate, Number_Periods):
+#EAR Formula Variables
+    Per_Period_Rate = float(input("What is the rate per period? "))
+    Number_Periods = float(input("How many periods are there? "))
+    P2 = Per_Period_Rate
+    N2 = Number_Periods
+#EAR Math
+    EAR =((((1+P2)**(N2))-1))
+    return print("%6.2f" % EAR,"%")
+
+
+HPR(10, 20, 2)
+                             
+
---
+++ Webapp.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, request
+from Finanical_Functions import EAR, HPR, APR
+
+# Configure the Flask Object
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['DEBUG'] = True
+
+# Index of the website
+@app.route('/')
+def index():
+    return "Hello World"
+
+# Main function
+if __name__ == "__main__":
+    app.run()
