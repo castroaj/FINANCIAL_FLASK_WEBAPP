2a8bec2de3b573da6c36b89943502600a4036e14
Kaleb627 <82231332+Kaleb627@users.noreply.github.com>
2021-04-11 13:03:28 -0400

    Update Finanical_Functions.py
    Removed old input and print statements no longer needed.

--- Finanical_Functions.py
+++ Finanical_Functions.py
@@ -1,8 +1,6 @@
+
 def HPR(Beginning_Price, End_Value, Dividend):
 #Holding Period Return Variables
-    # Beginning_Price = float(input("What was the purchase price of the security? ")
-    # End_Value = float(input("what was the sale price of the security? "))
-    # Dividend = float(input("How much in dividend payments were recieved? "))
     B = Beginning_Price
     if B < 0:
         B = abs(B)
@@ -19,30 +17,23 @@ def HPR(Beginning_Price, End_Value, Dividend):
     CG=((E-B)/B)
 #Dividend Yield Calculation
     DY=(D/B)
-    #return print("Holding Period Return is ","%6.2f" % HPR,"%")
     return HPR, CG, DY
 
 ##########################################
 def APR(Per_Period_Rate, Number_Periods):
 #APR Formula Variables
-    # Per_Period_Rate = float(input("What is the rate per period? "))
-    # Number_Periods= float(input("How many periods are there? "))
     P1 = Per_Period_Rate
     N1 = Number_Periods
 #APR Math
     APR = (P1*N1)
-    #return print("The APR is ","%6.2f" % APR,"%")
     return APR
 
 ##########################################
 
 def EAR(Per_Period_Rate, Number_Periods):
 #EAR Formula Variables
-    # Per_Period_Rate = float(input("What is the rate per period? "))
-    # Number_Periods = float(input("How many periods are there? "))
     P2 = Per_Period_Rate / 100
     N2 = Number_Periods
 #EAR Math
     EAR =((((1+P2)**(N2))-1))
-    #return print("%6.2f" % EAR,"%")
     return EAR * 100
